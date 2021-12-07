4510bf2843795d966c8d5fdfc54c34f9df8c4f1f
benbr8 <1520073+benbr8@users.noreply.github.com>
2021-12-07 22:21:55 +0100

    add gitpod

--- verilog_types/run_questa.sh
+++ verilog_types/run_questa.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 vlog -O4 hdl/dut.sv
-vsim -c -do ../.questa/questa.do -pli target/release/librstb_test.so dut
+vsim -c -do ../.questa/questa.do -pli ../target/release/librstb_test.so dut
