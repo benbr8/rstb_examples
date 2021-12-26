4a038fd33db073332dab8c2012f942fadd339253
benbr8 <1520073+benbr8@users.noreply.github.com>
2021-12-26 13:08:29 +0000

    update

--- test_dff/build.rs
+++ test_dff/build.rs
@@ -2,18 +2,26 @@ use std::env::var;
 use bindgen;
 
 fn main() {
+
     let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
     dbg!(&manifest_dir);
+
+    let vl_path;
+    if let Ok(p) = var("VL_PATH") {
+        vl_path = p; 
+    } else {
+        vl_path = "/usr/local/share/verilator".to_string();
+    }
     println!("cargo:rustc-link-search={}/verilator", manifest_dir);
-    println!("cargo:rustc-link-search=/usr/local/share/verilator/include");
+    println!("cargo:rustc-link-search={}/include", vl_path);
     println!("cargo:rustc-link-lib=static=wrapper");
     println!("cargo:rustc-link-lib=static=dut");
 
     
     let bindings = bindgen::Builder::default()
         .header("verilator/wrapper.cpp")
-        .clang_arg("-I/usr/local/share/verilator/include")
-        .clang_arg("-I/usr/local/share/verilator/include/vltstd")
+        .clang_arg(&format!("-I{}/include", vl_path))
+        .clang_arg(&format!("-I{}/include/vltstd", vl_path))
         .clang_arg("-Iverilator/vl_obj")
         .clang_arg("-x").clang_arg("c++")
         .allowlist_function("vl_init")
--- test_dff/verilator/build_dff_lib.sh
+++ test_dff/verilator/build_dff_lib.sh
@@ -1,18 +1,24 @@
+if [[ -z "${VL_PATH}" ]]; then
+  VL_PATH_INT="/usr/local/share/verilator"
+else
+  MY_SCRIPT_VARIABLE="${VL_PATH}"
+fi
+
 verilator -Mdir vl_obj --prefix Dut -cc ../hdl/dff.v ../hdl/not.v
-clang++ -DVL_DEBUG -c -o verilated.o -I vl_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ /usr/local/share/verilator/include/verilated.cpp
-clang++ -DVL_DEBUG -c -o verilated_vpi.o -I vl_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ /usr/local/share/verilator/include/verilated_vpi.cpp
-# clang++ -flto -DVL_DEBUG -c -o Vdff.o -I dff_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ dff_obj/Vdff.cpp
-# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__Slow.o -I dff_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ dff_obj/Vdff___024root__Slow.cpp
-# clang++ -flto -DVL_DEBUG -c -o Vdff__Syms.o -I dff_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ dff_obj/Vdff__Syms.cpp
-# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__DepSet_h998c48f4__0.o -I/usr/local/share/verilator/include/vltstd -I dff_obj -I/usr/local/share/verilator/include/ dff_obj/Vdff___024root__DepSet_h998c48f4__0.cpp
-# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__DepSet_h998c48f4__0__Slow.o -I/usr/local/share/verilator/include/vltstd -I dff_obj -I/usr/local/share/verilator/include/ dff_obj/Vdff___024root__DepSet_h998c48f4__0__Slow.cpp
-# clang++ -flto -DVL_DEBUG -c -o dff.o -I dff_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ dff.cpp 
+clang++ -DVL_DEBUG -c -o verilated.o -I vl_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ ${VL_PATH}/include/verilated.cpp
+clang++ -DVL_DEBUG -c -o verilated_vpi.o -I vl_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ ${VL_PATH}/include/verilated_vpi.cpp
+# clang++ -flto -DVL_DEBUG -c -o Vdff.o -I dff_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ dff_obj/Vdff.cpp
+# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__Slow.o -I dff_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ dff_obj/Vdff___024root__Slow.cpp
+# clang++ -flto -DVL_DEBUG -c -o Vdff__Syms.o -I dff_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ dff_obj/Vdff__Syms.cpp
+# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__DepSet_h998c48f4__0.o -I${VL_PATH}/include/vltstd -I dff_obj -I${VL_PATH}/include/ dff_obj/Vdff___024root__DepSet_h998c48f4__0.cpp
+# clang++ -flto -DVL_DEBUG -c -o Vdff___024root__DepSet_h998c48f4__0__Slow.o -I${VL_PATH}/include/vltstd -I dff_obj -I${VL_PATH}/include/ dff_obj/Vdff___024root__DepSet_h998c48f4__0__Slow.cpp
+# clang++ -flto -DVL_DEBUG -c -o dff.o -I dff_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ dff.cpp 
 
 cd vl_obj
 make -f Dut.mk
 cd ..
 
-clang++ -DVL_DEBUG -c -o wrapper.o -Ivl_obj -I/usr/local/share/verilator/include/vltstd -I/usr/local/share/verilator/include/ wrapper.cpp 
+clang++ -DVL_DEBUG -c -o wrapper.o -Ivl_obj -I${VL_PATH}/include/vltstd -I${VL_PATH}/include/ wrapper.cpp 
 ar rcs libwrapper.a wrapper.o verilated.o verilated_vpi.o
 cp vl_obj/Dut__ALL.a libdut.a
 
