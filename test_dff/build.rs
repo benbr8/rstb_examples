38e12e826e1c1e4de6ac37cadfeae554f26509f9
benbr8 <1520073+benbr8@users.noreply.github.com>
2022-02-05 12:15:21 +0100

    update

--- test_dff/build.rs
+++ test_dff/build.rs
@@ -1,5 +1,5 @@
 use std::env::var;
-use bindgen;
+// use bindgen;
 
 fn main() {
 
@@ -8,7 +8,7 @@ fn main() {
 
     let vl_path;
     if let Ok(p) = var("VL_PATH") {
-        vl_path = p; 
+        vl_path = p;
     } else {
         vl_path = "/usr/local/share/verilator".to_string();
     }
@@ -17,22 +17,22 @@ fn main() {
     println!("cargo:rustc-link-lib=static=wrapper");
     println!("cargo:rustc-link-lib=static=dut");
 
-    
-    let bindings = bindgen::Builder::default()
-        .header("verilator/wrapper.cpp")
-        .clang_arg(&format!("-I{}/include", vl_path))
-        .clang_arg(&format!("-I{}/include/vltstd", vl_path))
-        .clang_arg("-Iverilator/vl_obj")
-        .clang_arg("-x").clang_arg("c++")
-        .allowlist_function("vl_init")
-        .allowlist_function("vl_finalize")
-        .allowlist_function("get_signal_by_name")
-        .allowlist_function("vl_get_root_handle")
-        .allowlist_function("vl_got_finish")
-        .allowlist_function("vl_get_time")
-        .allowlist_function("vl_set_time")
-        .allowlist_function("vl_print_scope")
-        .generate()
-        .unwrap();
-    bindings.write_to_file(&format!("{}/src/vdff.rs", manifest_dir)).unwrap();
+
+    // let bindings = bindgen::Builder::default()
+    //     .header("verilator/wrapper.cpp")
+    //     .clang_arg(&format!("-I{}/include", vl_path))
+    //     .clang_arg(&format!("-I{}/include/vltstd", vl_path))
+    //     .clang_arg("-Iverilator/vl_obj")
+    //     .clang_arg("-x").clang_arg("c++")
+    //     .allowlist_function("vl_init")
+    //     .allowlist_function("vl_finalize")
+    //     .allowlist_function("get_signal_by_name")
+    //     .allowlist_function("vl_get_root_handle")
+    //     .allowlist_function("vl_got_finish")
+    //     .allowlist_function("vl_get_time")
+    //     .allowlist_function("vl_set_time")
+    //     .allowlist_function("vl_print_scope")
+    //     .generate()
+    //     .unwrap();
+    // bindings.write_to_file(&format!("{}/src/vdff.rs", manifest_dir)).unwrap();
 }
